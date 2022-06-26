881475b4ad355aad0049f4d61dc5e910efe0d915
Caio Ramos <caio.campsv@gmail.com>
2022-06-26 16:21:17 -0300

    feat: Add users domain and fake repository

---
+++ src/users/users.repository.js
@@ -0,0 +1,19 @@
+const User = require("./users.domain");
+
+module.exports = class UserRepository {
+    constructor() {
+        this.users = [];
+
+        for (let index = 1; index < 11; index++) {
+            this.users.push(new User(`Usuário ${index}`, `usuario${index}@basesocial.org`, "base@123"));
+        }
+    }
+
+    getAll() {
+        return this.users;
+    }
+
+    getByLoginAndPassword(login, password) {
+        return this.users.find((user) => user.login === login && user.password == password);
+    }
+}

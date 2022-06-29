79ba381f8361a619976084c7d81564e15d423ab4
Caio Ramos <caio.campsv@gmail.com>
2022-06-28 22:48:27 -0300

    feat: Add products endpoint to api

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const App = require("./src/app");
+
 const UsersController = require("./src/users/users.controller");
+const ProductsController = require("./src/products/products.controller");
 
 const port = parseInt(process.env.PORT || "3000");
 
-const app = new App(port, [new UsersController()]);
+const app = new App(port, [
+    new UsersController(),
+    new ProductsController(),
+]);
+
 app.start();
